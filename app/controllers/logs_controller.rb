058e69c3eb68646072f493d65a3e83afe89202f9
Kenneth Spatta <kenyounot@gmail.com>
2019-12-04 16:23:41 -0500

    finished new log form, with nested attributes for catch

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -1,18 +1,28 @@
 class LogsController < ApplicationController
-    def index
-        if user_signed_in?
-            @logs = User.find_by_id(current_user).logs
-            render logs_path
-        else
-            redirect_to  new_user_session_path  
-        end
-    end
 
     def new
         @log = Log.new
+        @log.build_catch
     end
 
     def create
+        log_params[user_id: current_user.id]
+        @log = Log.new(log_params)
+
+        if @log.save!
+            redirect_to log_path(@log)
+        else
+            render :new
+        end
+    end
+
+
+
+
+    private
+
+    def log_params
+        params.require(:log).permit(:start_date, :end_date, :location, :wind_direction, :wind_speed, :conditions, :precipitation, :water_clarity, :moon_phase, :air_temperature, :water_temperature, :barometer, :swell, :tide, :trip_rating, :comments, catch_attributes: [:species, :weight, :length, :bait])
     end
 
 
--- app/models/log.rb
+++ app/models/log.rb
@@ -1,4 +1,6 @@
 class Log < ApplicationRecord
   belongs_to :user
   belongs_to :catch
+
+  accepts_nested_attributes_for :catch
 end
