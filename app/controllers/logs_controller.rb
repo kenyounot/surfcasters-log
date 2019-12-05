b2ad3abfe62bf52d92ac5d0ee7d1248cd984a00b
Kenneth Spatta <kenyounot@gmail.com>
2019-12-04 19:39:54 -0500

    added authorization for index and show pages

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -1,5 +1,23 @@
 class LogsController < ApplicationController
+
+    def index
+        if user_signed_in?
+            @logs = User.find_by_id(current_user.id).logs
+        else
+            redirect_to new_user_session_path
+        end
+    end
+
     def show
-        @log = Log.find_by_id(params[:id])
+            log = Log.find_by_id(params[:id])
+            if user_signed_in?
+                if log.user_ids[0] == current_user.id
+                    @log = log
+                else
+                    redirect_to logs_path
+                end
+            else
+                redirect_to new_user_session_path
+            end
     end
 end
